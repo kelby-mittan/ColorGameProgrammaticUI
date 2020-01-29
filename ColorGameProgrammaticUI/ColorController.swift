b44487b991231a808107757418da658a233d11d9
Kelby Mittan <kelbymittan@pursuit.org>
2020-01-28 20:11:19 -0500

    got all of my logic working, just need to implement a score

--- ColorGameProgrammaticUI/ColorController.swift
+++ ColorGameProgrammaticUI/ColorController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+enum RGB {
+    case red
+    case green
+    case blue
+}
+
 class ColorController: UIViewController {
-
-    private let mainView = MainView()
     
-    private let gameModel = ColorGuessingModel()
-
+    private let mainView = MainView()
     private var color = UIColor()
+    
+    private var randomRed = CGFloat.random(in: 0...1)
+    private var randomGreen = CGFloat.random(in: 0...1)
+    private var randomBlue = CGFloat.random(in: 0...1)
+    private var maxColor = CGFloat()
+    private var maxColorString = String()
     
     override func loadView() {
         view = mainView
@@ -22,19 +31,68 @@ class ColorController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let color = gameModel.getNewColor()
+        
+        color = ranColorGenerator()
         mainView.backgroundColor = .white
         mainView.colorView.backgroundColor = color
         
         mainView.redButton.addTarget(self, action: #selector(redButtonPressed(_:)), for: .touchUpInside)
+        mainView.greenButton.addTarget(self, action: #selector(greenButtonPressed(_:)), for: .touchUpInside)
+        mainView.blueButton.addTarget(self, action: #selector(blueButtonPressed(_:)), for: .touchUpInside)
+    }
+    
+    private func ranColorGenerator() -> UIColor {
+        randomRed = CGFloat.random(in: 0...1)
+        randomGreen = CGFloat.random(in: 0...1)
+        randomBlue = CGFloat.random(in: 0...1)
+        let colorArr = [randomRed, randomGreen, randomBlue]
+        maxColor = colorArr.max() ?? 0
+        
+        if maxColor == randomRed {
+            maxColorString = "red"
+        } else if maxColor == randomGreen {
+            maxColorString = "green"
+        } else {
+            maxColorString = "blue"
+        }
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: CGFloat.random(in: 0.5...1))
     }
     
     @objc
     private func redButtonPressed(_ sender: UIButton) {
-        gameModel.isDominant(guess: <#T##UIColor#>)
+        if maxColorString == "red" {
+            print("correct")
+            color = ranColorGenerator()
+            mainView.colorView.backgroundColor = color
+        } else {
+            print("wrong")
+        }
+    }
+    
+    @objc
+    private func greenButtonPressed(_ sender: UIButton) {
+        if maxColorString == "green" {
+            print("correct")
+            color = ranColorGenerator()
+            mainView.colorView.backgroundColor = color
+        } else {
+            print("wrong")
+        }
+//        checkColor()
     }
 
-
+    @objc
+    private func blueButtonPressed(_ sender: UIButton) {
+        if maxColorString == "blue" {
+            print("correct")
+            color = ranColorGenerator()
+            mainView.colorView.backgroundColor = color
+        } else {
+            print("wrong")
+        }
+//        checkColor()
+    }
+    
+    
 }
 
